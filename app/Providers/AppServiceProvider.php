f47da304847d9544b95a1d776a65eba6135820c2
naycip <naycipp@gmail.com>
2026-04-17 22:38:08 +0700

    fix: force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('local')) {
-            URL::forceScheme('http');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
