8a771059fba10f08e25d4010a55581b4ea578c19
naycip <naycipp@gmail.com>
2026-04-18 09:58:42 +0700

    fix: force https for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
