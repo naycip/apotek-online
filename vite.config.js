7dc390dacda15f359254598662e299c7841beeb8
naycip <naycipp@gmail.com>
2026-04-18 10:41:05 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,16 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/css/app.css', 
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
     ],
+    // TAMBAHKAN BAGIAN INI:
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
